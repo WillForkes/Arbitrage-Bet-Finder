bf88001222cdf1ab5ac93c2b4726ff7b4cb17ffd
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-03-10 17:52:12 +0000

    FAQ page

---
+++ odds-client/src/pages/FAQ/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function FAQ() {
+  return (
+    <div>
+        <h1>FAQ</h1>
+        <ul>
+            <li>Q: What is arbitrage betting?</li>
+            <li>A: Arbitrage betting is a betting strategy that involves placing bets on all possible outcomes of an event to guarantee a profit. It is also known as arbitrage trading or surebetting.</li>
+            <li>Q: What is Arbster betting service?</li>
+            <li>A: Arbster betting service is a tool that helps bettors to identify arbitrage opportunities quickly and efficiently. The service scans different bookmakers' odds and identifies situations where the combined odds for all possible outcomes of an event are lower than 100%, which means a guaranteed profit can be made.</li>
+            <li>Q: How does Arbster work?</li>
+            <li>A: Our arbitrage betting service uses advanced algorithms to scan and compare odds from multiple bookmakers in real-time. It identifies any discrepancies and highlights profitable opportunities. The service is easy to use and requires no manual input from the user.</li>
+            <li>Q: How is Arbster different from others on the market?</li>
+            <li>A: Our arbitrage betting service is competitively priced and has better functionality compared to many other arbitrage opportunity services. We continuously update our service to ensure it remains cutting-edge and effective. We also provide excellent customer support and assistance to our clients.</li>
+            <li>Q: Is arbitrage betting legal?</li>
+            <li>A: Yes, arbitrage betting is legal in most countries. However, some bookmakers may not allow it</li>
+            <li>Q: Is your service suitable for beginners?</li>
+            <li>A: Yes, our service is user-friendly and easy to use, making it suitable for beginners. We also provide comprehensive tutorials and support to help users get started with arbitrage betting.</li>
+            <li>Q: Can your service guarantee a profit?</li>
+            <li>A: (Yes, when used correctly.) While our service can identify profitable opportunities, we cannot guarantee a profit. The success of arbitrage betting depends on many factors, including the odds offered by bookmakers, the size of the bets, and the fees charged by payment processors. It is important to understand the risks involved and to bet responsibly.</li>
+            <li>Q: How much does your service cost?</li>
+            <li>A: Our service is competitively priced and starts at $39.99 per month. We also offer a 7-day free trial.</li>
+        </ul>
+
+    </div>
+  );
+}

5890db572f964811a8f58b5a956cbe3971961c15
infecting <al3x9a@protonmail.com>
2023-03-11 00:57:27 -0500

    auth to next

--- odds-client/src/components/BetLoader.tsx
+++ odds-client/src/components/BetLoader.tsx
@@ -14,17 +14,25 @@ export default function BetLoader({ b }: props) {
   }
   return (
     <>
-      <div className="match">
-        <h3>{b.data.match_name}</h3>
-        <p>{((1 - b.data.total_implied_odds) * 100).toFixed(2)}%</p>
-        <div>
-          {Object.keys(b.data.best_outcome_odds).map((key, index) => (
-            <div className="outcome" key={index}>
-              <span>{b.data.best_outcome_odds[key][0]} </span>
-              <b>{b.data.best_outcome_odds[key][1]}</b>
-            </div>
-          ))}
-          <button onClick={() => setModal(true)}>Calculate Stake Amounts</button>
+      <div className="bg-grey-600 rounded-xl grid grid-cols-3 grid-rows-3 px-3 h-48">
+        <div className="col-span-3 grid grid-cols-3">
+          <h1>Match Name</h1>
+          <h1>Profit</h1>
+          <h1>Books</h1>
+        </div>
+        <div className="col-span-3 grid grid-cols-3">
+          <h3>{b.data.match_name}</h3>
+          <p>{((1 - b.data.total_implied_odds) * 100).toFixed(2)}%</p>
+          <div>
+            {Object.keys(b.data.best_outcome_odds).map((key, index) => (
+              <div className="col-span-3" key={index}>
+                <span>{b.data.best_outcome_odds[key][0]} </span>
+              </div>
+            ))}
+            <button onClick={() => setModal(true)}>
+              Calculate Stake Amounts
+            </button>
+          </div>
         </div>
       </div>
       <Modal isVisible={modal} closeModal={closeModal} id={b.id} />
--- odds-client/src/pages/scraper/bets.tsx
+++ odds-client/src/pages/scraper/bets.tsx
@@ -10,7 +10,11 @@ export default function bets() {
     <div>
       {data ? <h1>Loaded {data.length}</h1> : null}
       {data ? (
-        data.map((bet: Bet) => <BetLoader b={bet} key={bet.id} />)
+        data.map((bet: Bet) => (
+          <div className="grid gap-6 grid-cols-1 2xl:grid-cols-2 mb-6">
+            <BetLoader b={bet} key={bet.id} />
+          </div>
+        ))
       ) : (
         <p>loading</p>
       )}

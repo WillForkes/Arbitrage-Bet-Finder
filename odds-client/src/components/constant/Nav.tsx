d4b53874dadc22fe914ffbc5653a589dffa77a49
infecting <al3x9a@protonmail.com>
2023-03-12 12:35:52 -0400

    login button

--- odds-client/src/components/constant/Nav.tsx
+++ odds-client/src/components/constant/Nav.tsx
@@ -38,7 +38,7 @@ export default function Nav() {
         </div>
         <div>
           <Link
-            href="http://localhost:3000/login"
+            href="/auth/login"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
           >
             Login

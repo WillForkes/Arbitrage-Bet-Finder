57026c05c7caa6929b9c83a4dc7658394ce23373
infecting <al3x9a@protonmail.com>
2023-03-11 19:05:41 -0500

    scraper tool design

--- odds-client/src/components/BetLoader.tsx
+++ odds-client/src/components/BetLoader.tsx
@@ -1,5 +1,4 @@
 import { Bet } from "@/types";
-import Link from "next/link";
 import React, { useState } from "react";
 import Modal from "./Modal";
 
@@ -14,7 +13,7 @@ export default function BetLoader({ b }: props) {
   }
   return (
     <>
-      <div className="bg-grey-600 rounded-xl grid grid-cols-3 grid-rows-3 px-3 h-48">
+      <div className="bg-grey-600 px-.5 rounded-xl grid grid-cols-3 grid-rows-3 px-3 h-48">
         <div className="col-span-3 grid grid-cols-3">
           <h1>Match Name</h1>
           <h1>Profit</h1>
@@ -24,19 +23,23 @@ export default function BetLoader({ b }: props) {
           <h3>{b.data.match_name}</h3>
           <p>{((1 - b.data.total_implied_odds) * 100).toFixed(2)}%</p>
           <div>
-            {Object.keys(b.data.best_outcome_odds).map((key, index) => (
-              <div className="col-span-3" key={index}>
-                <span>{b.data.best_outcome_odds[key][0]} </span>
-              </div>
-            ))}
-            <button onClick={() => setModal(true)}>
-              Calculate Stake Amounts
-            </button>
+            <div className="col-span-3 grid grid-cols-3">
+              {Object.keys(b.data.best_outcome_odds).map((key, index) => (
+                <span key={index}>{b.data.best_outcome_odds[key][0]} </span>
+              ))}
+            </div>
           </div>
+        </div>
+        <div className="flex justify-center items-center col-start-1 row-span-1 col-span-3 mb-2 content-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => setModal(true)}
+          >
+            Calculate Stake
+          </button>
         </div>
       </div>
       <Modal isVisible={modal} closeModal={closeModal} id={b.id} />
-      <hr></hr>
     </>
   );
 }

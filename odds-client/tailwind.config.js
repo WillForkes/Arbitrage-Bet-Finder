4fa987eedd6dd90194366882e12acf0a428c4598
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-03-15 17:48:58 +0000

    design

--- odds-client/tailwind.config.js
+++ odds-client/tailwind.config.js
@@ -9,7 +9,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            primary: {
+                '50': '#ebfaff',
+                '100': '#d1f5ff',
+                '200': '#aeeeff',
+                '300': '#76e6ff',
+                '400': '#35d5ff',
+                '500': '#07b5ff',
+                '600': '#0090ff',
+                '700': '#0077ff',
+                '800': '#0062d7',
+                '900': '#004f99'
+            }
+        }
+    },
   },
   plugins: [],
 }

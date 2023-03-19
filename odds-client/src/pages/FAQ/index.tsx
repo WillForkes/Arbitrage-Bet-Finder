3d79a61f1db7801054c12dea57a8d8f169411dc7
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-03-19 16:46:59 +0000

    FAQ

--- odds-client/src/pages/FAQ/index.tsx
+++ odds-client/src/pages/FAQ/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import Image from "next/image";
+import { Accordion } from "flowbite-react";
 
 export default function FAQ() {
   return (
     <>
         <section className="bg-white dark:bg-gray-900">
-            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                 <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">We didn't reinvent the wheel</h2>
                     <p className="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
@@ -15,6 +16,31 @@ export default function FAQ() {
                     <img height={100} width={100} className="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
                     <img height={100} width={100} className="mt-4 w-full lg:mt-10 rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-1.png" alt="office content 2" />
                 </div>
+            </div>
+
+            <div className="mx-auto max-w-screen-lg">
+                <Accordion>
+                    <Accordion.Panel>
+                        <Accordion.Title>
+                        Question 1
+                        </Accordion.Title>
+                        <Accordion.Content>
+                            <p className="mb-2 text-gray-500 dark:text-gray-400">
+                                Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.
+                            </p>
+                        </Accordion.Content>
+                    </Accordion.Panel>
+                    <Accordion.Panel>
+                        <Accordion.Title>
+                        Question 2
+                        </Accordion.Title>
+                        <Accordion.Content>
+                            <p className="mb-2 text-gray-500 dark:text-gray-400">
+                                Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.
+                            </p>
+                        </Accordion.Content>
+                    </Accordion.Panel>
+                </Accordion>
             </div>
         </section>
     </>

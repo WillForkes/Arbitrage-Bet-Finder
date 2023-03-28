6cec18f955f2b7ab385648f41e279ed5789dfe4e
infecting <al3x9a@protonmail.com>
2023-03-28 10:32:27 -0500

    x

--- server/prisma/migrations/20230305190342_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "authid" TEXT NOT NULL PRIMARY KEY,
-    "plan" TEXT NOT NULL DEFAULT 'Free',
-    "planExpiresAt" DATETIME,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);

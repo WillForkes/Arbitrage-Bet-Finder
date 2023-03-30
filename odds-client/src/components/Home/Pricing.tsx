0d05976c459166c5951e3cf16d5075a92f863bd7
infecting <al3x9a@protonmail.com>
2023-03-30 14:57:41 -0500

    betloader

--- odds-client/src/components/Home/Pricing.tsx
+++ odds-client/src/components/Home/Pricing.tsx
@@ -134,7 +134,7 @@ export default function Pricing() {
               onClick={() => subscriptionHandle("starter")}
               className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
             >
-              Get Started
+              Try it Free
             </button>
           </div>
 
@@ -295,7 +295,7 @@ export default function Pricing() {
               onClick={() => subscriptionHandle("pro")}
               className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
             >
-              Get Started
+              Try it Free
             </button>
           </div>
 
@@ -503,7 +503,7 @@ export default function Pricing() {
               onClick={() => subscriptionHandle("plus")}
               className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
             >
-              Get Started
+              Try it Free
             </button>
           </div>
         </div>

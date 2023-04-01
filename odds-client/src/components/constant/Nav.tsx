00f9aa52b6adcfd2b2d713150a50db4a5b210698
infecting <al3x9a@protonmail.com>
2023-04-01 12:35:54 -0500

    hamburger

--- odds-client/src/components/constant/Nav.tsx
+++ odds-client/src/components/constant/Nav.tsx
@@ -3,13 +3,19 @@ import { User } from "@/types";
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Logo from "../../../public/arbster.png";
 import Login from "./Login";
 import ProfileDropdown from "./ProfileDropdown";
-import { Badge } from "flowbite-react";
+import { Hamburger } from "flowbite-react";
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   const user: User | null = useContext(UserContext).user;
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
@@ -75,6 +81,71 @@ export default function Nav() {
               </Link>
             </li>
           </ul>
+        </div>
+        <div className="flex items-center justify-end md:hidden">
+          <button
+            onClick={() => toggleMenu()}
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          {isOpen && (
+            <div id="navbar-hamburger" className="fixed top-10 z-50">
+              <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                <li>
+                  <Link
+                    href="/"
+                    onClick={() => toggleMenu()}
+                    className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded dark:bg-blue-600"
+                    aria-current="page"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => toggleMenu()}
+                    href="/scraper/bets"
+                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Bet Scraper
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => toggleMenu()}
+                    href="/scraper/ev"
+                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    EV Tool
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    onClick={() => toggleMenu()}
+                    href="/tracker"
+                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                  >
+                    Bet Tracker
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </nav>

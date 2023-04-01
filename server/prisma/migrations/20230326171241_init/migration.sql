4cba603cf76ce5a9147474531bf3fb49b187a8ff
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-04-01 15:30:23 +0100

    reset db

--- server/prisma/migrations/20230326171241_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[bookName]` on the table `Bookmaker` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Bookmaker_bookName_key" ON "Bookmaker"("bookName");

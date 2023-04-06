b964adfccd0e7d36448efa3ead3812db597c1f29
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-04-06 19:20:27 +0100

    hero btn

--- odds-client/src/components/Home/Hero.tsx
+++ odds-client/src/components/Home/Hero.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Badge, Tooltip } from "flowbite-react";
+import { UserContext } from "@/pages/_app";
 import Link from "next/link";
+import { User } from "@/types";
+
 
 export default function Hero() {
+    const user: User | null = useContext(UserContext).user;
+
   return (
     <section className="bg-white dark:bg-gray-900 py-24">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
@@ -17,7 +23,11 @@ export default function Hero() {
             </p>
             <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                 <a href="#pricing" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                    Try for free
+                    {user?.dbuser.plan != "free" || user?.dbuser.staff == true ? (
+                        "View Pricing"
+                    ) : (
+                        "Try for free"
+                    )}
                     <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" ></path></svg>
                 </a>
                 <a href="#pricing" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">

2b9561ba853721a1abf9bef570cd741cebaef4cf
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-04-07 17:55:00 +0100

    Merge remote-tracking branch 'origin/alex' into will

--- odds-client/src/components/EV.tsx
+++ odds-client/src/components/EV.tsx
@@ -229,10 +229,7 @@ export default function EVLoader({ bets, showBets, user }: props) {
               {pricing ? <FreeModal closeModal={closePricing} /> : null}
               <tbody className={`divide ${showBets ? "" : "blur"}`}>
                 {paginatedBets.map((bet) => (
-                  <tr
-                    key={bet.id}
-                    className="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700"
-                  >
+                  <tr key={bet.id} className="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
                     <Tooltip
                       content={`Win probability: ${
                         showBets
@@ -240,10 +237,7 @@ export default function EVLoader({ bets, showBets, user }: props) {
                           : "?.??%"
                       }%`}
                     >
-                      <th
-                        scope="row"
-                        className="items-center px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
+                      <th scope="row" className="items-center px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {bet.data?.winProbability > 0.6 ? (
                           <div className="inline-block w-4 h-4 mr-2 bg-green-700 rounded-full"></div>
                         ) : bet.data?.winProbability > 0.35 ? (
@@ -258,9 +252,8 @@ export default function EVLoader({ bets, showBets, user }: props) {
                           {showBets
                             ? bet.data.match_name
                             : "HOME TEAM v AWAY TEAM"}{" "}
-                          -{" "}
-                          {showBets ? bet.data.region.toUpperCase() : "REGION"}
-                          <div className=" text-xs dark:text-primary-600">
+                            - {showBets ? bet.data.region.toUpperCase() : "REGION"}
+                            <div className="text-xs dark:text-primary-600">
                             {showBets
                               ? bet.data.leagueFormatted
                               : "LEAGUE_FORMATTED"}

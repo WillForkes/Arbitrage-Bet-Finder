1311d53dce69efc4a44e6ea4e5b9f69736f937e6
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-04-09 16:52:18 +0100

    .

--- odds-client/src/pages/blog/1.tsx
+++ odds-client/src/pages/blog/1.tsx
@@ -5,7 +5,7 @@ export default function blog1() {
     return (
     <>
         <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
-            <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+            {/* <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
                 <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
                     <header className="mb-4 lg:mb-6 not-format">
                         <address className="flex items-center mb-6 not-italic">
@@ -182,7 +182,7 @@ export default function blog1() {
                     </p>
                     
                 </article>
-            </div>
+            </div> */}
         </main>
     </>
     );

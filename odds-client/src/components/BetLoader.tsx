0aff15119e2f27a83a61ba09ced2adacb21f0f12
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-04-19 16:03:39 +0100

    Update BetLoader.tsx

--- odds-client/src/components/BetLoader.tsx
+++ odds-client/src/components/BetLoader.tsx
@@ -351,7 +351,7 @@ export default function BetLoader({ bets, showBets }: props) {
 
           {!showBets ? (
             <Link href="/#pricing">
-                <Image width={700} height={700} src={OverlayImage} alt="your-image-alt" className=" absolute top-72 left-1/2 transform -translate-x-1/2 z-10" />
+                <Image width={700} height={700} src={OverlayImage} alt="your-image-alt" className="rounded-md absolute top-72 left-1/2 transform -translate-x-1/2 z-10" />
             </Link>
             ) : null}
         </div>

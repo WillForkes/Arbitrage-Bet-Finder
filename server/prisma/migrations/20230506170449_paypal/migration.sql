7888af7b0e12c463379f4ca49a11ca43ea33537c
Will Forkes <35146567+WillForkes@users.noreply.github.com>
2023-05-07 18:32:48 +0100

    Update migration.sql

--- server/prisma/migrations/20230506170449_paypal/migration.sql
+++ server/prisma/migrations/20230506170449_paypal/migration.sql
@@ -1,48 +1,9 @@
--- CreateTable
-CREATE TABLE "User" (
-    "authid" TEXT NOT NULL,
-    "whitelist" TEXT NOT NULL DEFAULT '[]',
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "region" TEXT DEFAULT 'UK',
-    "phone" TEXT,
-    "email" TEXT,
-    "smsNotifications" BOOLEAN NOT NULL DEFAULT false,
-    "emailNotifications" BOOLEAN NOT NULL DEFAULT false,
-    "banned" BOOLEAN NOT NULL DEFAULT false,
-    "staff" BOOLEAN NOT NULL DEFAULT false,
-    "trialActivated" BOOLEAN NOT NULL DEFAULT false,
-    "promotions" BOOLEAN NOT NULL DEFAULT true,
+-- Remove apikey and affiliateCode columns from User table
+ALTER TABLE "User" DROP COLUMN "apikey";
+ALTER TABLE "User" DROP COLUMN "affiliateCode";
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("authid")
-);
-
--- CreateTable
-CREATE TABLE "Bet" (
-    "id" SERIAL NOT NULL,
-    "type" TEXT NOT NULL DEFAULT 'arbitrage',
-    "data" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Bet_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "PlacedBets" (
-    "id" SERIAL NOT NULL,
-    "userId" TEXT NOT NULL,
-    "type" TEXT NOT NULL DEFAULT 'arbitrage',
-    "status" INTEGER NOT NULL DEFAULT 0,
-    "matchName" TEXT NOT NULL,
-    "totalStake" DOUBLE PRECISION NOT NULL,
-    "profitPercentage" DOUBLE PRECISION NOT NULL,
-    "bookmakers" TEXT NOT NULL DEFAULT '[]',
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "PlacedBets_pkey" PRIMARY KEY ("id")
-);
+DROP TABLE "Invoice";
+DROP TABLE "Subscription";
 
 -- CreateTable
 CREATE TABLE "Subscription" (
@@ -58,34 +19,9 @@ CREATE TABLE "Subscription" (
     CONSTRAINT "Subscription_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "SignupDeal" (
-    "id" SERIAL NOT NULL,
-    "deal" TEXT NOT NULL,
-    "link" TEXT NOT NULL,
-    "expiresAt" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "SignupDeal_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "Bookmaker" (
-    "id" SERIAL NOT NULL,
-    "bookName" TEXT NOT NULL,
-
-    CONSTRAINT "Bookmaker_pkey" PRIMARY KEY ("id")
-);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Subscription_paypalSubscriptionId_key" ON "Subscription"("paypalSubscriptionId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Bookmaker_bookName_key" ON "Bookmaker"("bookName");
-
--- AddForeignKey
-ALTER TABLE "PlacedBets" ADD CONSTRAINT "PlacedBets_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("authid") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Subscription" ADD CONSTRAINT "Subscription_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("authid") ON DELETE RESTRICT ON UPDATE CASCADE;

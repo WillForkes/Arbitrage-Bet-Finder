76936672793bf4e666b9a510acc29b10274a1476
infecting <al3x9a@protonmail.com>
2023-05-19 09:53:50 -0700

    status to admin

--- odds-client/src/pages/admin/user/[id].tsx
+++ odds-client/src/pages/admin/user/[id].tsx
@@ -13,6 +13,16 @@ export default function UserPage() {
     refreshInterval: 10000,
   });
 
+  const { data: subData, error: subError } = useSWR(
+    "/payment/get-subscription/" +
+      (data?.user ? data.user.subscription[0].paypalSubscriptionId : ""),
+    getter,
+    {
+      refreshInterval: 10000,
+    }
+  );
+  console.log(subData);
+
   // parse bet data
   let user: User["dbuser"] = data?.user;
   if (error || !user) {
@@ -46,6 +56,9 @@ export default function UserPage() {
               </label>
               {user.subscription.map((sub) => (
                 <div key={sub.id} className="flex flex-col gap-2">
+                  <p className="text-gray-900 dark:text-white">
+                    STATUS: {subData ? subData.status : "LOADING..."}
+                  </p>
                   <p className="text-gray-900 dark:text-white">
                     {sub.paypalSubscriptionId}
                   </p>
